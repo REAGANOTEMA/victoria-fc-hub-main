0b92bb92964c727c0a3bfa39d74b50bdccc4d971
REAGANOTEMA <reaganotema2022@gmail.com>
2026-03-02 21:11:24 -0800

    updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
+// src/main.tsx
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // ✅ Import BrowserRouter
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
